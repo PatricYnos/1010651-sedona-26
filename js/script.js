383d4a1927a3865924bc389f144b6078feb53054
Patric Ynos <patric.ynos@gmail.com>
2019-11-01 17:51:26 +0300

    Правки к защите
    Исправления в соответствии с замечаниями наставника

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 var hideForm=document.querySelector(".order-form-inner");
-hideForm.classList.toggle("visually-hidden");
+hideForm.classList.toggle("order-form-inner-hide");
 var link=document.querySelector(".form-hide");
 link.addEventListener("click", function(evt) {
   evt.preventDefault();
   var form=document.querySelector(".order-form-inner");
-  form.classList.toggle("visually-hidden");
+  form.classList.toggle("order-form-inner-hide");
+  form.classList.toggle("order-form-inner-show");
 });
